04811aac04c3fc3d1d6d2bd0dce04da8423c405a
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-03-07 07:56:08 -0300

    perfil

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import "./header.css";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+//importando arquivos
+import avatarImg from "../../assets/avatar.png";
+import { AuthContext } from "../../contexts/auth";
+
+
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+
+
+export default function Header(){
+    const {user} = useContext(AuthContext);
+
+    return(
+        <div className="sidebar" >
+            <div>
+                <img src={user.avatarUrl === null? avatarImg : user.avatarUrl} />
+            </div>
+
+            <Link to={'/dashboard'} >
+                <FiHome color="#fff" size={24} />
+                Chamados
+            </Link>
+            <Link to={'/customers'} >
+                <FiUser color="#fff" size={24} />
+                Clientes
+            </Link>
+            <Link to={'/profile'} >
+                <FiSettings color="#fff" size={24} />
+                Perfil
+            </Link>
+        </div>
+    )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,8 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
+import Header from "../../components/Header";
+
 
 
 export default function Dashboard(){
@@ -13,6 +15,7 @@ export default function Dashboard(){
 
     return (
         <>
+        <Header/>
          <h1>Dashboard</h1>
          <button onClick={handleLogout} >Sair</button>
         </>
