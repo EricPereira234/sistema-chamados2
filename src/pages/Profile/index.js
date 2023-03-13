fc160981b1cb5c31c85a6b3378e686ea1ecd71b6
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-03-13 07:47:32 -0300

    title

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,11 +4,17 @@ import "./profile.css";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 
+import { FiSettings } from "react-icons/fi";
+
 export default function Profile(){
     return(
         <div>
             <Header/>
-            <div className="content" ><Title/></div>
+            <div className="content" >
+                <Title name={'Minha conta'} >
+                    <FiSettings size={25} />
+                </Title>
+            </div>
             Profile
         </div>
     )
