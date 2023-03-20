10a6f16c90366501a4953b8f67dce594bbbfdd9d
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-03-20 07:49:22 -0300

    Customers

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
+import Customers from "../pages/Customers";
 import Profile from "../pages/Profile";
 import Private from "./Private";
 
@@ -15,6 +16,7 @@ export default function RoutesApp(){
             <Route path="/register"  element={<SignUp/>} />
             <Route path="/dashboard" element={<Private><Dashboard/></Private>} />
             <Route path="/profile" element={<Private><Profile/></Private>} />
+            <Route path="/customers" element={<Private><Customers/></Private>} />
             <Route path="*" element={<h1>Erro</h1>} />
         </Routes>
     )
